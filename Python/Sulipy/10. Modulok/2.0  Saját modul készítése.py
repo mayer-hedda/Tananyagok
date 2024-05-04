b70aa897e28d096bc6199f38e02af33eb83e4a35
Mayer Hedda <mayer.hedda@gmail.com>
2024-05-04 02:47:50 +0200

    2024.05.04.

--- Python/Sulipy/10. Modulok/2.0  Saját modul készítése.py
+++
@@ -1,8 +0,0 @@
-from kor import kerulet as k_kerulet, terulet as k_terulet
-from negyzet import *
-
-print(f'Kör kerülete: {k_kerulet(10):.2f}')
-print(f'Kör területe: {k_terulet(10):.2f}')
-
-print(f'Négyzet kerülete: {kerulet(10):.2f}')
-print(f'Négyzet területe: {terulet(10):.2f}')
--- Python/Sulipy/8. Fájlkezelés/1.3 Extra.py
+++
@@ -1,4 +0,0 @@
-with open('1.0 Fájl tartalmának beolvasása.csv', 'r', encoding='utf-8') as forrasfajl:
-    print(type(forrasfajl))
-    print(dir(forrasfajl))
-    print(help(forrasfajl))
